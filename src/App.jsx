e9b9fd2736c44ac9ac11233eae631f48d5e3b26d
L33t-HAXXOR <31870368+L33t-HAXXOR@users.noreply.github.com>
2022-07-26 00:14:48 -0500

    Initial commit. Template alpha.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h4 className="initialText">let's party</h4>
+        
+    </div>
+  )
+}
+
+export default App
